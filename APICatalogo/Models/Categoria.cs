692a4a2861b8535e7f1093a25cdc7220020409dd
Alan Souza <alann.oficial3015@gmail.com>
2024-11-12 05:20:49 -0300

    Testando o controlador CategoriasController

--- APICatalogo/Models/Categoria.cs
+++ APICatalogo/Models/Categoria.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections.ObjectModel;
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
+using System.Text.Json.Serialization;
 
 namespace APICatalogo.Models;
 
@@ -21,5 +22,7 @@ public class Categoria
     [Required]
     [StringLength(300)]
     public string? ImagemUrl { get; set; }
+
+    [JsonIgnore]
     public ICollection<Produto>? Produtos { get; set; }
 }
