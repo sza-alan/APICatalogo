e54b6978e032399dccdbc08f5c1594b18c3d29c4
Alan Souza <alann.oficial3015@gmail.com>
2024-11-16 07:39:07 -0300

    Implementando a paginação

--- APICatalogo/Controllers/ProdutosController.cs
+++ APICatalogo/Controllers/ProdutosController.cs
@@ -1,5 +1,6 @@
 ﻿using APICatalogo.DTOs;
 using APICatalogo.Models;
+using APICatalogo.Pagination;
 using APICatalogo.Repositories;
 using AutoMapper;
 using Microsoft.AspNetCore.JsonPatch;
@@ -27,6 +28,16 @@ public class ProdutosController : ControllerBase
 
         if (produtos is null)
             return NotFound();
+
+        var produtosDto = _mapper.Map<IEnumerable<ProdutoDTO>>(produtos);
+
+        return Ok(produtosDto);
+    }
+
+    [HttpGet("pagination")]
+    public ActionResult<IEnumerable<ProdutoDTO>> Get([FromQuery] ProdutosParameters produtosParameters)
+    {
+        var produtos = _unitOfWork.ProdutoRepository.GetProdutos(produtosParameters);
 
         var produtosDto = _mapper.Map<IEnumerable<ProdutoDTO>>(produtos);
 
