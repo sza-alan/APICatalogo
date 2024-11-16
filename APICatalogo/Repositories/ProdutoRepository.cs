56c32cb3c6d0028dc8b9444bbe9e9d9393d9ed92
Alan Souza <alann.oficial3015@gmail.com>
2024-11-16 08:35:34 -0300

    Incrementando a paginação : Implementação

--- APICatalogo/Repositories/ProdutoRepository.cs
+++ APICatalogo/Repositories/ProdutoRepository.cs
@@ -10,13 +10,20 @@ namespace APICatalogo.Repositories
         {
         }
 
-        public IEnumerable<Produto> GetProdutos(ProdutosParameters produtosParams)
+        //public IEnumerable<Produto> GetProdutos(ProdutosParameters produtosParams)
+        //{
+        //    return GetAll()
+        //        .OrderBy(p => p.Nome)
+        //        .Skip((produtosParams.PageNumber - 1) * produtosParams.PageSize)
+        //        .Take(produtosParams.PageSize)
+        //        .ToList();
+        //}
+
+        public PagedList<Produto> GetProdutos(ProdutosParameters produtosParams)
         {
-            return GetAll()
-                .OrderBy(p => p.Nome)
-                .Skip((produtosParams.PageNumber - 1) * produtosParams.PageSize)
-                .Take(produtosParams.PageSize)
-                .ToList();
+            var produtos = GetAll().OrderBy(p => p.ProdutoId).AsQueryable();
+            var produtosOrdenados = PagedList<Produto>.ToPagedList(produtos, produtosParams.PageNumber, produtosParams.PageSize);
+            return produtosOrdenados;
         }
 
         public IEnumerable<Produto> GetProdutosPorCategoria(int id)
