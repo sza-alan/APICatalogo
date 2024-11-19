4bce44e68b1f1a314c71dae82fa821fa84cf2813
Alan Souza <alann.oficial3015@gmail.com>
2024-11-19 06:04:54 -0300

    Configurando o Identity e gerando as tabelas

--- APICatalogo/Program.cs
+++ APICatalogo/Program.cs
@@ -1,11 +1,9 @@
 using APICatalogo.Context;
 using APICatalogo.DTOs.Mappings;
-using APICatalogo.Extensions;
 using APICatalogo.Filters;
 using APICatalogo.Logging;
 using APICatalogo.Repositories;
-using APICatalogo.Services;
-using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 using System.Text.Json.Serialization;
 
@@ -19,25 +17,22 @@ builder.Services.AddControllers(options =>
 .AddJsonOptions(options =>
 {
     options.JsonSerializerOptions.ReferenceHandler = ReferenceHandler.IgnoreCycles;
-}).AddNewtonsoftJson();
+})
+.AddNewtonsoftJson();
 
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
-builder.Services.AddAuthorization();
-builder.Services.AddAuthentication("Bearer").AddJwtBearer();
+builder.Services.AddIdentity<IdentityUser, IdentityRole>()
+    .AddEntityFrameworkStores<AppDbContext>()
+    .AddDefaultTokenProviders();
 
-string mySqlConnection = builder.Configuration.GetConnectionString("DefaultConnection");
-
-var valor1 = builder.Configuration["chave1"];
-var valor2 = builder.Configuration["secao1:chave2"];
+string mySqlConnection = builder.Configuration
+                                .GetConnectionString("DefaultConnection");
 
 builder.Services.AddDbContext<AppDbContext>(options =>
-                 options.UseMySql(mySqlConnection, 
-                 ServerVersion.AutoDetect(mySqlConnection)));
-
-builder.Services.AddTransient<IMeuServico, MeuServico>();
+                    options.UseMySql(mySqlConnection,
+                    ServerVersion.AutoDetect(mySqlConnection)));
 
 builder.Services.AddScoped<ApiLoggingFilter>();
 
@@ -45,11 +40,6 @@ builder.Services.AddScoped<ICategoriaRepository, CategoriaRepository>();
 builder.Services.AddScoped<IProdutoRepository, ProdutoRepository>();
 builder.Services.AddScoped(typeof(IRepository<>), typeof(Repository<>));
 builder.Services.AddScoped<IUnitOfWork, UnitOfWork>();
-
-builder.Services.Configure<ApiBehaviorOptions>(options =>
-{
-    options.DisableImplicitFromServicesParameters = true;
-});
 
 builder.Logging.AddProvider(new CustomLoggerProvider(new CustomLoggerProviderConfiguration
 {
@@ -65,13 +55,9 @@ if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
     app.UseSwaggerUI();
-    app.ConfigureExceptionHandler();
 }
 
 app.UseHttpsRedirection();
-
 app.UseAuthorization();
-
 app.MapControllers();
-
 app.Run();
